081aae892dc850b452c65c16266ea8c5aae958de
Antonio <antoniomarcosdefa@gmail.com>
2024-03-25 19:02:05 -0300

    projeto java

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,9 @@
+package br.com.erudio;
+
+public class Greeting {
+
+	public Greeting() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/GreetingController.java
@@ -0,0 +1,9 @@
+package br.com.erudio;
+
+public class GreetingController {
+
+	public GreetingController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
